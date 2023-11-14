898caa964e6e199da7991f59906a5e143a6adb4c
leeseungjun <cjstkrak@gmail.com>
2023-11-14 20:15:11 +0900

    feat: getBoard, creatBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private readonly boardService: BoardsService) {}
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+}
+
+// type Board = {
+//   title: string;
+//   text: string;
+// };
