ada47332431d040cfc2bafa3cb69c9f038bd9fa4
leeseungjun <cjstkrak@gmail.com>
2023-11-16 14:32:08 +0900

    fix: user-> auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { typeORMConfig } from './configs/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
