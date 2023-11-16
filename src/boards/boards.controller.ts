74b62ec2a60cb9418e4f6086edd563e3e170f274
leeseungjun <cjstkrak@gmail.com>
2023-11-16 14:26:14 +0900

    feat: board CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -8,21 +8,17 @@ import {
   Patch,
   UsePipes,
   ValidationPipe,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 import { Board } from './board.entity';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { BoardStatus } from './board-status.enum';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardService: BoardsService) {}
-
-  // @Get()
-  // getAllBoards(): Board[] {
-  //   return this.boardService.getAllBoards();
-  // }
 
   @Get(':id')
   getBoardById(@Param('id') id: number): Promise<Board> {
@@ -35,28 +31,51 @@ export class BoardsController {
     return this.boardService.createBoard(createBoardDto);
   }
 
-  // @Get(':id')
-  // getBoardById(@Param('id') id: string): Board {
-  //   return this.boardService.getBoardById(id);
-  // }
+  @Delete(':id')
+  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
+    return this.boardService.deleteBoard(id);
+  }
 
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-  //   return this.boardService.createBoard(createBoardDto);
-  // }
+  @Patch(':id')
+  upadetBoard(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ) {
+    return this.boardService.updateBoardById(id, status);
+  }
 
-  // @Delete(':id')
-  // deleteBoard(@Param('id') id: string): void {
-  //   this.boardService.deleteBoard(id);
-  // }
-
-  // @Patch(':id/status')
-  // // 파이프를 여기서 쓸수도있다.
-  // updateBoardStatus(
-  //   @Param('id') id: string, // 파라미터에 파이프를 달 수도있다.
-  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  // ): Board {
-  //   return this.boardService.updateBoard(id, status);
-  // }
+  @Get()
+  getAllBoards(): Promise<Board[]> {
+    return this.boardService.getAllBoards();
+  }
 }
+
+// @Get()
+// getAllBoards(): Board[] {
+//   return this.boardService.getAllBoards();
+// }
+
+// @Get(':id')
+// getBoardById(@Param('id') id: string): Board {
+//   return this.boardService.getBoardById(id);
+// }
+
+// @Post()
+// @UsePipes(ValidationPipe)
+// createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+//   return this.boardService.createBoard(createBoardDto);
+// }
+
+// @Delete(':id')
+// deleteBoard(@Param('id') id: string): void {
+//   this.boardService.deleteBoard(id);
+// }
+
+// @Patch(':id/status')
+// // 파이프를 여기서 쓸수도있다.
+// updateBoardStatus(
+//   @Param('id') id: string, // 파라미터에 파이프를 달 수도있다.
+//   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+// ): Board {
+//   return this.boardService.updateBoard(id, status);
+// }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,37 +4,61 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
 import { Repository } from 'typeorm';
+import { BoardStatus } from './board-status.enum';
+import { stat } from 'fs';
 
 @Injectable()
 export class BoardsService {
   constructor(private boardRepository: BoardRepository) {}
-  // getAllBoards(): Board[] {
-  //   return this.boards;
-  // }
-  // createBoard(createBoardDto: CreateBoardDto): Board {
-  //   const board: Board = {
-  //     id: uuid(),
-  //     ...createBoardDto,
-  //     status: BoardStatus.public,
-  //   };
-  //   this.boards.push(board);
-  //   return board;
-  // }
   async getBoardById(id: number): Promise<Board> {
     const found = await this.boardRepository.findOne({ where: { id } });
     if (!found) throw new NotFoundException('아이디가 없다');
     return found;
   }
+
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException('없는 아이디입니다.');
+    }
+    return;
+  }
+
   createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardRepository.createBoard(createBoardDto);
   }
-  // deleteBoard(id: string): void {
-  //   const found = this.getBoardById(id);
-  //   this.boards = this.boards.filter((board) => board.id !== found.id);
-  // }
-  // updateBoard(id: string, status: BoardStatus): Board {
-  //   const board = this.getBoardById(id);
-  //   board.status = status;
-  //   return board;
-  // }
+
+  async updateBoardById(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
+    board.status = status;
+    await this.boardRepository.save(board);
+    return board;
+  }
+
+  async getAllBoards(): Promise<Board[]> {
+    return this.boardRepository.find();
+  }
 }
+
+// getAllBoards(): Board[] {
+//   return this.boards;
+// }
+// createBoard(createBoardDto: CreateBoardDto): Board {
+//   const board: Board = {
+//     id: uuid(),
+//     ...createBoardDto,
+//     status: BoardStatus.public,
+//   };
+//   this.boards.push(board);
+//   return board;
+// }
+// deleteBoard(id: string): void {
+//   const found = this.getBoardById(id);
+//   this.boards = this.boards.filter((board) => board.id !== found.id);
+// }
+// updateBoard(id: string, status: BoardStatus): Board {
+//   const board = this.getBoardById(id);
+//   board.status = status;
+//   return board;
+// }
